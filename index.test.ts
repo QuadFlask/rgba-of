7c2887f0518b69c54793e688606c55c8da5ce630
QuadFlask <pop9310@gmail.com>
2019-10-25 01:31:20 +0900

    Add alpha add/sub

--- index.test.ts
+++ index.test.ts
@@ -1,4 +1,4 @@
-import rgba, {rgbaOf} from "./index";
+import rgba, {rgbaOf, toRgba} from "./index";
 
 it('rgba', function () {
     expect(rgba`#102030,0.2`).toEqual("rgba(16,32,48,0.2)");
@@ -12,6 +12,8 @@ it('rgba', function () {
     expect(rgba`red,30%`).toEqual("rgba(255,0,0,0.3)");
 
     expect(rgba`#f00,30%`).toEqual("rgba(255,0,0,0.3)");
+    expect(rgba`#800,30%`).toEqual("rgba(136,0,0,0.3)");
+    expect(rgba`#109,30%`).toEqual("rgba(17,0,153,0.3)");
     expect(rgba`#fff,30%`).toEqual("rgba(255,255,255,0.3)");
     expect(rgba`#fff,.3`).toEqual("rgba(255,255,255,0.3)");
 
@@ -32,12 +34,58 @@ it('rgbaOf', function () {
     expect(rgba2`secondary,25%`).toEqual("rgba(0,255,0,0.25)");
 });
 
+it('rgba alpha add/sub', function () {
+    expect(rgba`rgba(1,2,3,0.1),+0.2`).toEqual("rgba(1,2,3,0.3)");
+    expect(rgba`rgba(1,2,3,0.1),-0.1`).toEqual("rgba(1,2,3,0)");
+    expect(rgba`rgba(1,2,3,0.1),-0.2`).toEqual("rgba(1,2,3,0)");
+    expect(rgba`rgba(1,2,3,1),-0.1`).toEqual("rgba(1,2,3,0.9)");
+    expect(rgba`rgba(1,2,3,1),+0.1`).toEqual("rgba(1,2,3,1)");
+    expect(rgba`rgba(1,2,3,0.5),+10%`).toEqual("rgba(1,2,3,0.6)");
+});
+
 it('unsupported formats', function () {
-    expect(rgba`#f0,30%`).toEqual(null);
-    expect(rgba`,30%`).toEqual(null);
-    expect(rgba`30%`).toEqual(null);
-    expect(rgba`asd`).toEqual(null);
-    expect(rgba``).toEqual(null);
-    expect(rgba`#f00,,`).toEqual(null);
-    expect(rgba`#f00,asd%`).toEqual(null);
+    expect(rgba`#f0,30%`).toBeUndefined();
+    expect(rgba`,30%`).toBeUndefined();
+    expect(rgba`30%`).toBeUndefined();
+    expect(rgba`asd`).toBeUndefined();
+    expect(rgba``).toBeUndefined();
+    expect(rgba`#f00,,`).toBeUndefined();
+    expect(rgba`#f00,asd%`).toBeUndefined();
+});
+
+it('toRgba', function () {
+    // #RRGGBB
+    expect(toRgba("#ff0000")!.rgba).toEqual("rgba(255,0,0,1)");
+    expect(toRgba("#ffff00")!.rgba).toEqual("rgba(255,255,0,1)");
+    expect(toRgba("#ffffff")!.rgba).toEqual("rgba(255,255,255,1)");
+    expect(toRgba("#FFFFFF")!.rgba).toEqual("rgba(255,255,255,1)");
+
+    // #AARRGGBB
+    expect(toRgba("#ffff0000")!.rgba).toEqual("rgba(255,0,0,1)");
+    expect(toRgba("#00ff0000")!.rgba).toEqual("rgba(255,0,0,0)");
+    expect(toRgba("#20ffff00")!.rgba).toEqual("rgba(255,255,0,0.125)");
+
+    // #RGB
+    expect(toRgba("#f00")!.rgba).toEqual("rgba(255,0,0,1)");
+    expect(toRgba("#f00")!.rgba).toEqual("rgba(255,0,0,1)");
+
+    // #ARGB
+    expect(toRgba("#3f00")!.rgba).toEqual("rgba(255,0,0,0.2)");
+    expect(toRgba("#4f00")!.rgba).toEqual("rgba(255,0,0,0.266)");
+
+    // rgb(r,g,b)
+    expect(toRgba("rgb(255,0,0)")!.rgba).toEqual("rgba(255,0,0,1)");
+    expect(toRgba("RGB(255,0,0)")!.rgba).toEqual("rgba(255,0,0,1)");
+
+    // rgba(r,g,b,a)
+    expect(toRgba("rgba(255,0,0,1)")!.rgba).toEqual("rgba(255,0,0,1)");
+    expect(toRgba("rgba(255,0,0,0.5)")!.rgba).toEqual("rgba(255,0,0,0.5)");
+    expect(toRgba("rgba(255,0,0,0.11)")!.rgba).toEqual("rgba(255,0,0,0.11)");
+    expect(toRgba("rgba(255,0,0,0)")!.rgba).toEqual("rgba(255,0,0,0)");
+    expect(toRgba("rgba( 255, 0, 0, 0)")!.rgba).toEqual("rgba(255,0,0,0)");
+    expect(toRgba("RGBA( 255, 0, 0, 0)")!.rgba).toEqual("rgba(255,0,0,0)");
+
+    // color names
+    expect(toRgba("red")!.rgba).toEqual("rgba(255,0,0,1)");
+    expect(toRgba("blue")!.rgba).toEqual("rgba(0,0,255,1)");
 });
--- index.ts
+++ index.ts
@@ -1,21 +1,79 @@
-import colorString from "color-string";
+import * as colorName from "color-name";
 
-export function rgbaOf(customColors: { [colorName: string]: string }): (strings: TemplateStringsArray, ...keys: any[]) => string | null {
+export function rgbaOf(customColors: { [colorName: string]: string }): (strings: TemplateStringsArray, ...keys: any[]) => string | undefined {
     return (strings: TemplateStringsArray, ...keys: any[]) => {
-        const [colorPart, opacityPart] = joinWithValues(strings.raw, keys).split(",");
-        const customColor = customColors[colorPart];
-        const color = colorString.get(customColor || colorPart);
+        const rawString = joinWithValues(strings.raw, keys);
+        const i = rawString.lastIndexOf(",");
+        const colorPart = rawString.substr(0, i);
+        const opacityPart = rawString.substr(i + 1);
+        const colorValue = customColors[colorPart] || colorPart;
+        const color = toRgba(colorValue);
         if (color) {
-            const [r, g, b] = color.value;
-            const result = opacityPart.match(/([\d.]+(%)?)/);
+            let {r, g, b, a} = color;
+            const result = opacityPart.match(/([+-]?)([\d.]+)(%?)/);
             if (result) {
-                const [_, alphaString, isPercentile] = result;
-                const alpha = isPercentile ? parseFloat(alphaString) / 100 : parseFloat(alphaString);
-                return `rgba(${r},${g},${b},${alpha})`;
+                const [_, sign, alphaString, isPercentile] = result;
+                const alpha = isPercentile ? p(alphaString) / 100 : p(alphaString);
+                if (sign) {
+                    if (sign === "+") a += alpha;
+                    else if (sign === "-") a -= alpha;
+                } else {
+                    a = alpha;
+                }
+                return `rgba(${r},${g},${b},${p(Math.max(0, Math.min(a, 1)))})`;
             }
         }
-        return null;
+        return undefined;
     }
+}
+
+export function toRgba(s: string): { r: number, g: number, b: number, a: number, rgba: string } | undefined {
+    let r, g, b, a = 1;
+    if (s.startsWith("#")) {
+        const hex = parseInt(s.substr(1), 16);
+        if (s.length === 4) {
+            r = (hex >> 8 & 0xf) * 0x11;
+            g = (hex >> 4 & 0xf) * 0x11;
+            b = (hex & 0xf) * 0x11;
+        } else if (s.length === 5) {
+            a = (hex >> 12 & 0xf) * 0x11 / 255.0;
+            r = (hex >> 8 & 0xf) * 0x11;
+            g = (hex >> 4 & 0xf) * 0x11;
+            b = (hex & 0xf) * 0x11;
+        } else if (s.length === 7) {
+            r = hex >> 16 & 0xff;
+            g = hex >> 8 & 0xff;
+            b = hex & 0xff;
+        } else if (s.length === 9) {
+            a = (hex >> 24 & 0xff) / 255.0;
+            r = hex >> 16 & 0xff;
+            g = hex >> 8 & 0xff;
+            b = hex & 0xff;
+        } else {
+            return undefined;
+        }
+    } else if (s.toLowerCase().startsWith("rgba")) {
+        [r, g, b, a] = s.substring(5, s.length - 1).split(',').map(s => parseFloat(s.trim()));
+    } else if (s.toLowerCase().startsWith("rgb")) {
+        [r, g, b] = s.substring(4, s.length - 1).split(',').map(s => parseFloat(s.trim()));
+    } else {
+        // @ts-ignore
+        const color = colorName[s];
+        if (color) {
+            [r, g, b] = color;
+        } else {
+            return undefined;
+        }
+    }
+    return {
+        r, g, b, a: p(a),
+        rgba: `rgba(${r},${g},${b},${p(a)})`
+    };
+}
+
+function p(n: any) {
+    if (!n) return 0;
+    return Math.floor(parseFloat(n) * 1000) / 1000;
 }
 
 const joinWithValues = (a: readonly string[], keys: any[]) => a.reduce((a, b, i) => {
